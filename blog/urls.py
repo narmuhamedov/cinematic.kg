2f31c5d9229a1ad6cae7b67243d415a1e3d61da7
rzpy1997 <kingradomir@gmail.com>
2023-10-12 20:23:48 +0600

    'lesson1'

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('post_link/', views.postView, name='posts'),
+    path('date/', views.get_curent_data),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from . import models
+from datetime import datetime
+
 
 def postView(request):
     post_value = models.Post.objects.all()
     context = {
         'post_key': post_value,
     }
-    return render(request, 'post.html', context)
+    template_name = 'post.html'
+    return render(request, template_name, context)
